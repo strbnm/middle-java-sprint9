218f5cc9b8390117a9dcb19f337a4e7cc16f6f70
Pavel <strbnm@yandex.ru>
2025-05-21 21:11:29 +0300

    Рефакторинг ReactiveScheduler - бизнес-логика вынесена в отдельный метод для возможности тестирования

--- notifications-service/src/main/java/ru/strbnm/notifications_service/service/ReactiveScheduler.java
+++ notifications-service/src/main/java/ru/strbnm/notifications_service/service/ReactiveScheduler.java
@@ -6,16 +6,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.strbnm.notifications_service.repository.NotificationRepository;
 
 @Slf4j
 @Component
 public class ReactiveScheduler {
 
-    /*
-    Компонент для имитации отправки уведомлений на электронную почту. Сообщения выводятся в лог.
-    Для шедулинга с заданным интервалом используется Flux.interval().
-    */
+  /*
+  Компонент для имитации отправки уведомлений на электронную почту. Сообщения выводятся в лог.
+  Для шедулинга с заданным интервалом используется Flux.interval().
+  */
 
   private final NotificationRepository notificationRepository;
   private final int limit;
@@ -27,21 +28,29 @@ public class ReactiveScheduler {
     this.limit = limit;
   }
 
+  public Mono<Void> processNotifications() {
+    return notificationRepository
+        .findUnsentLimited(limit)
+        .doOnNext(
+            notification ->
+                log.info(
+                    "Отправка сообщения на электронную почту {}: {}",
+                    notification.getEmail(),
+                    notification.getMessage()))
+        .map(
+            n -> {
+              n.setSent(true);
+              return n;
+            })
+        .collectList()
+        .flatMapMany(notificationRepository::saveAll)
+        .then();
+  }
+
   @PostConstruct
   public void scheduleTask() {
     Flux.interval(Duration.ofSeconds(60), Duration.ofSeconds(30))
-        .flatMap(
-            tick ->
-                notificationRepository
-                    .findUnsentLimited(limit)
-                    .doOnNext(notification -> log.info("Отправка сообщения на электронную почту {}: {}", notification.getEmail(), notification.getMessage()))
-                    .map(
-                        n -> {
-                          n.setSent(true);
-                          return n;
-                        })
-                    .collectList()
-                    .flatMapMany(notificationRepository::saveAll))
+        .flatMap(tick -> processNotifications())
         .subscribe(); // запускаем поток
   }
 }
