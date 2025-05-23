f7a8ff67a92e90ef90d63c04f4eed3a68f3624a8
Pavel <strbnm@yandex.ru>
2025-05-23 22:01:43 +0300

    Добавлены тесты с Stub Runner для проверки контракта между exchange-generator и exchange-service

---
+++ exchange-generator/src/test/java/ru/strbnm/exchange_generator/service/ExchangeGeneratorStubTest.java
@@ -0,0 +1,33 @@
+package ru.strbnm.exchange_generator.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.context.ActiveProfiles;
+import reactor.test.StepVerifier;
+
+@ActiveProfiles("test")
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {
+                "spring.config.name=application-test",
+                "spring.rest.exchange-service.url=http://localhost:8080"
+        })
+@AutoConfigureStubRunner(
+    ids = "ru.strbnm:exchange-service:+:stubs:8080",
+    stubsMode = StubRunnerProperties.StubsMode.LOCAL
+)
+class ExchangeGeneratorStubTest {
+
+    @Autowired
+    private ExchangeGenerator exchangeGenerator;
+
+    @Test
+    void shouldSendRatesSuccessfully() {
+        StepVerifier.create(exchangeGenerator.generateAndSendRates())
+            .expectNext("Success") // тело ответа, как указано в контракте
+            .verifyComplete();
+    }
+}
