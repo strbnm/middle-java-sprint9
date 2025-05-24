a58c2d0b2eac933c89e44a29d786441b0548b540
Pavel <strbnm@yandex.ru>
2025-05-24 13:13:47 +0300

    Добавлена заготовка для blocker-service

---
+++ blocker-service/src/main/java/ru/strbnm/blocker_service/BlockerApplication.java
@@ -0,0 +1,13 @@
+package ru.strbnm.blocker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockerApplication.class, args);
+    }
+
+}
---
+++ blocker-service/src/main/java/ru/strbnm/blocker_service/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package ru.strbnm.blocker_service.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Slf4j
+@Profile("!contracts & !test")
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class WebSecurityConfig {
+
+  @Bean
+  SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+    return http.oauth2ResourceServer(
+            resourceServer -> resourceServer.jwt(Customizer.withDefaults()))
+        .authorizeExchange(
+            exchange ->
+                exchange
+                    .pathMatchers("/api/v1/check_transaction")
+                    .hasAuthority("SCOPE_resource.read")
+                    .anyExchange()
+                    .authenticated())
+        .csrf(ServerHttpSecurity.CsrfSpec::disable)
+        .build();
+  }
+}
