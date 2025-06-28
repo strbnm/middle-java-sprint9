c9054acad6022b048bc4fe5d9a1c68e0f77b9e7b
Pavel <strbnm@yandex.ru>
2025-06-28 12:00:33 +0300

    Обновление AuthenticationEventsListener

--- front-ui/src/main/java/ru/strbnm/front_ui/config/AuthenticationEventsListener.java
+++ front-ui/src/main/java/ru/strbnm/front_ui/config/AuthenticationEventsListener.java
@@ -1,17 +1,21 @@
 package ru.strbnm.front_ui.config;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AbstractAuthenticationEvent;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class AuthenticationEventsListener implements ApplicationListener<AbstractAuthenticationEvent> {
 
     private final MeterRegistry meterRegistry;
 
+    @Autowired
     public AuthenticationEventsListener(MeterRegistry meterRegistry) {
         this.meterRegistry = meterRegistry;
     }
@@ -25,8 +29,10 @@ public class AuthenticationEventsListener implements ApplicationListener<Abstrac
         }
 
         if (event instanceof AuthenticationSuccessEvent) {
+            log.info("AuthenticationSuccessEvent: {}", username);
             meterRegistry.counter("user.login.attempts", "username", username, "outcome", "success").increment();
         } else if (event instanceof AbstractAuthenticationFailureEvent) {
+            log.info("AuthenticationFailureEvent: {}", username);
             meterRegistry.counter("user.login.attempts", "username", username, "outcome", "failure").increment();
         }
     }
